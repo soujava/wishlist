f1ea0c9eb906d6fa94d3f05fe9a16664ba486aac
otaviojava <otaviopolianasantana@gmail.com>
2019-12-19 17:13:47 -0300

    creates test

---
+++ src/test/java/sh/platform/template/WishTest.java
@@ -0,0 +1,15 @@
+package sh.platform.template;
+
+import jakarta.nosql.mapping.Column;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WishTest {
+
+    @Test
+    public void shouldCreateWish() {
+
+    }
+
+}
